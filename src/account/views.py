5752976b89f1532e2f807a1e9f8645f44f2d3c22
Vyacheslav <dedmopoz.bakemono@gmail.com>
2021-05-13 19:40:39 +0300

    add new app account add new model User for saving users add buttons delete for Post and Authors only for authenticated users

---
+++ src/account/views.py
@@ -0,0 +1,13 @@
+from account.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+
+class MyProfile(LoginRequiredMixin, UpdateView):
+    queryset = User.objects.filter(is_active=True)
+    fields = ('first_name', 'last_name',)
+    success_url = reverse_lazy('home_page')
+
+    def get_object(self, queryset=None):
+        return self.request.user
--- src/main/middlewares.py
+++ src/main/middlewares.py
@@ -12,7 +12,7 @@ class LogMiddleware:
 
         response = self.get_response(request)
 
-        utm = request.GET.get("utm")
+        utm = request.GET.get('utm')
         time_execution = time() - st
         ip = get_client_ip(request)
 
@@ -20,18 +20,18 @@ class LogMiddleware:
             logger = Logger(time_execution=time_execution, path=request.path, utm=utm, ip=ip)
             logger.save()
         else:
-            logger = Logger(time_execution=time_execution, path=request.path, utm="", ip=ip)
+            logger = Logger(time_execution=time_execution, path=request.path, utm='', ip=ip)
             logger.save()
 
         return response
 
 
 def get_client_ip(request):
-    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(",")[-1].strip()
-    elif request.META.get("HTTP_X_REAL_IP"):
-        ip = request.META.get("HTTP_X_REAL_IP")
+        ip = x_forwarded_for.split(',')[-1].strip()
+    elif request.META.get('HTTP_X_REAL_IP'):
+        ip = request.META.get('HTTP_X_REAL_IP')
     else:
-        ip = request.META.get("REMOTE_ADDR")
+        ip = request.META.get('REMOTE_ADDR')
     return ip
